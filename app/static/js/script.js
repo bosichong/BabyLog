abcb87ccbdc6df0ac37148c52a2ab2d92f1fbade
J.sky <bosichong@qq.com>
2021-05-11 09:54:25 +0800

    update .gitignore

--- app/static/js/script.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    function render_time() {
-        return moment($(this).data('timestamp')).format('lll')
-    }
-    $('[data-toggle="tooltip"]').tooltip(
-        {title: render_time}
-    );
-});
--- app/test.py
+++
@@ -1,3 +0,0 @@
-# from werkzeug.security import generate_password_hash, check_password_hash
-# print(generate_password_hash('阿斯蒂芬斯蒂芬'))
-
